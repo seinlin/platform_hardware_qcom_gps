c72bd2852c6fc101d60e2f21c629a22b8edd692a
Kevin Tang <zhikait@codeaurora.org>
2021-02-13 23:17:41 -0800

    redesigned opt in propagation paths
    Change-Id: I6e0ca264c651fc87c31f48b36d5c50056bff73f1
    CRs-Fixed: 2877946

--- location/LocationAPI.cpp
+++ location/LocationAPI.cpp
@@ -905,14 +905,15 @@ uint32_t LocationControlAPI::configRobustLocation(bool enable, bool enableForE91
 }
 
 uint32_t LocationControlAPI::setOptInStatus(bool userConsent) {
-    void* libHandle = nullptr;
-    uint32_t sessionId = 0;
-    setOptInStatusGetter* setter = (setOptInStatusGetter*)dlGetSymFromLib(libHandle,
-            "liblocationservice_glue.so", "setOptInStatus");
-    if (setter != nullptr) {
-        sessionId = (*setter)(userConsent, &gData.controlCallbacks.responseCb);
+    uint32_t id = 0;
+    pthread_mutex_lock(&gDataMutex);
+
+    if (gData.gnssInterface != NULL) {
+        id = gData.gnssInterface->setOptInStatus(userConsent);
     } else {
-        LOC_LOGe("dlGetSymFromLib failed for liblocationservice_glue.so");
+        LOC_LOGe("No gnss interface available for Location Control API");
     }
-    return sessionId;
+
+    pthread_mutex_unlock(&gDataMutex);
+    return id;
 }
--- location/location_interface.h
+++ location/location_interface.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2017-2020 The Linux Foundation. All rights reserved.
+/* Copyright (c) 2017-2021 The Linux Foundation. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -98,6 +98,7 @@ struct GnssInterface {
     uint32_t (*configLeverArm)(const LeverArmConfigInfo& configInfo);
     uint32_t (*configRobustLocation)(bool enable, bool enableForE911);
     bool (*isSS5HWEnabled)();
+    uint32_t (*setOptInStatus)(bool userConsent);
 };
 
 struct BatchingInterface {
