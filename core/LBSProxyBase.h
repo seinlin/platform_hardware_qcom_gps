094bb5607362b0549c2f4a050f7815639d32665e
Nilesh Gharde <ngharde@codeaurora.org>
2021-04-23 15:20:59 +0530

    Z-axis support implementation
    By design, in order to get location with Z, 
    We blindly call LBSProxy API populateAltitudeAndBroadCast.
    
    If IzatProvider is not available then we would get 
    back the location without Z from LBSProxy.
    
    If IzatProvider is available. We send
    the location to Izatprovider to get the location with Z,
    given Z-provider is connected.
    
    Change-Id: Ia71019f085645a349f386bacc614f1938ee3435b
    CRs-fixed: 2943684

--- core/LBSProxyBase.h
+++ core/LBSProxyBase.h
@@ -29,7 +29,7 @@
 #ifndef IZAT_PROXY_BASE_H
 #define IZAT_PROXY_BASE_H
 #include <gps_extended.h>
-
+#include <LocationDataTypes.h>
 namespace loc_core {
 
 class LocApiBase;
@@ -61,6 +61,10 @@ public:
     }
     inline virtual bool hasNativeXtraClient() const { return false; }
     inline virtual IzatDevId_t getIzatDevId() const { return 0; }
+    inline virtual void populateAltitudeAndBroadCast(Location location,
+            trackingCallback cb) const {
+        cb(location);
+    };
 };
 
 typedef LBSProxyBase* (getLBSProxy_t)();
